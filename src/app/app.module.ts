c8f800ce50ae6d126082948c23ed874f7e044fd7
Alexandre Pinheiro <alexclemente1985@gmail.com>
2023-09-08 23:34:32 -0300

    Header e banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from './features/header/header.module';
+import { BannerModule } from './features/banner/banner.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    BannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
