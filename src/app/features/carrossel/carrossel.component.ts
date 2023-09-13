0c1fcac01f12681bcc15f7f9c90d3034f735c247
Alexandre Pinheiro <alexclemente1985@gmail.com>
2023-09-13 15:07:56 -0300

    Criação de slide de livros

--- src/app/features/carrossel/carrossel.component.ts
+++ src/app/features/carrossel/carrossel.component.ts
@@ -6,6 +6,11 @@ import { register } from 'swiper/element/bundle';
 import { SwiperOptions } from 'swiper/types';
 import { Navigation, Pagination, A11y, Mousewheel } from 'swiper/modules';
 
+interface IBookImg{
+  src: string;
+  alt: string;
+}
+
 @Component({
   selector: 'app-carrossel',
   standalone: true,
@@ -19,6 +24,8 @@ import { Navigation, Pagination, A11y, Mousewheel } from 'swiper/modules';
 })
 export class CarrosselComponent {
 
+  slider!: HTMLElement | null;
+
   sliders: string[] = [
     'Test 1',
     'Test 2',
@@ -31,25 +38,96 @@ export class CarrosselComponent {
     'Test 9',
   ]
 
+  imgSliders: IBookImg[] = [
+    {
+      src: '../../../assets/images/svg/books/ApacheKafka.svg',
+      alt: 'Livro sobre Apache Kafka'
+    },
+    {
+      src: '../../../assets/images/svg/books/Arquitetura.svg',
+      alt: 'Livro sobre Arquitetura Javascript'
+    },
+    {
+      src: '../../../assets/images/svg/books/Construct2.svg',
+      alt: 'Livro sobre Construct2'
+    },
+    {
+      src: '../../../assets/images/svg/books/Gestao.svg',
+      alt: 'Livro sobre Gestão'
+    },
+    {
+      src: '../../../assets/images/svg/books/Gestao2.svg',
+      alt: 'Livro sobre Gestão 2'
+    },
+    {
+      src: '../../../assets/images/svg/books/Lideranca.svg',
+      alt: 'Livro sobre Liderança'
+    },
+    {
+      src: '../../../assets/images/svg/books/MetricasAgeis.svg',
+      alt: 'Livro sobre Métricas Ágeis'
+    },
+    {
+      src: '../../../assets/images/svg/books/Nodejs.svg',
+      alt: 'Livro sobre NodeJS'
+    },
+    {
+      src: '../../../assets/images/svg/books/Portugol.svg',
+      alt: 'Livro sobre Portugol'
+    },
+    {
+      src: '../../../assets/images/svg/books/ReactNative.svg',
+      alt: 'Livro sobre React Native'
+    },
+    {
+      src: '../../../assets/images/svg/books/Tuning.svg',
+      alt: 'Livro sobre Tuning'
+    },
+    {
+      src: '../../../assets/images/svg/books/UX.svg',
+      alt: 'Livro sobre UX'
+    },
+  ];
+
+  constructor(){}
+
   ngOnInit(){
     register();
+    this.slider = document.getElementById('book-swiper');
+    //this.setBannerImgs(); //imagens não permitem ajustes dessa maneira
   }
   ngAfterViewInit(){
+    //this.setBannerImgStyles();
     const swiperEl = document.querySelector('swiper-container');
     const buttonEl = document.querySelector('button');
 
   // swiper parameters
   const swiperParams = {
-    slidesPerView: 1,
-    /* breakpoints: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    modules: [Navigation, Pagination],
+    /* pagination: {
+      //el: ".swiper-pagination",
+     // dynamicBullets: true,
+    }, */
+    // inject modules styles to shadow DOM
+    injectStylesUrls: [
+      'swiper/element/css/navigation',
+      'swiper/element/css/pagination',
+      'swiper/element/css/scrollbar'
+    ],
+    breakpoints: {
       640: {
         slidesPerView: 2,
+        spaceBetween: 10
       },
       1024: {
         slidesPerView: 3,
+        spaceBetween: 10
       },
     },
-    on: {
+    
+    /* on: {
       init() {
         // ...
       },
@@ -77,6 +155,25 @@ export class CarrosselComponent {
     }
   }
   
+  }
+
+  setBannerImgs(){
+    if(!!this.slider){
+      this.imgSliders.map((img, index) => {
+        let newSlide = `<swiper-slide class='img-container'><img id='slide-img-${index}'  [style.width]="'100%'" src='${img.src}' alt='${img.alt}'></swiper-slide>`;
+        this.slider?.insertAdjacentHTML( 'beforeend', newSlide );
+        
+      })
+    }
+    
+  }
+
+  setBannerImgStyles(){
+    this.imgSliders.map((img, index)=>{
+      const imgSlide = document.getElementById(`slide-img-${index}`);
+      imgSlide?.classList.add('slider-img');
+    })
+    
   }
 
   /* public config: SwiperOptions = {
